a9aa59cc6e0de9ffd2bb34bfb971c89aceab9374
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2025-10-15 21:08:25 +0530

    Fix deployment issues

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -24,7 +24,7 @@ router.get(
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.BACKEND_ENV === "production",
+      secure: false,
       sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
