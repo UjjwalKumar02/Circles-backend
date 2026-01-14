cd86140f478c62f735c66f86d7da38214cec817b
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2026-01-14 14:41:02 +0530

    feat: add websockets

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,16 @@ import passport from "./utils/config/passport.js";
 import userRouter from "./routes/userRoutes.js";
 import postRouter from "./routes/postRoutes.js";
 import communityRouter from "./routes/communityRoutes.js";
+import { createServer } from "http";
+import { WebSocketServer } from "ws";
+import type WebSocket from "ws";
 
 const app = express();
 const port = process.env.PORT;
+
+// Websocket server
+const server = createServer(app);
+const wss = new WebSocketServer({ server });
 
 app.use(passport.initialize());
 app.use(
@@ -25,6 +32,58 @@ app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
 app.use("/api/community", communityRouter);
 
-app.listen(port, () => {
+// Websocket handling
+interface User {
+  ws: WebSocket;
+  roomId: string;
+}
+
+let users: User[] = [];
+
+wss.on("connection", (socket) => {
+  socket.on("message", (e) => {
+    let parsedData = JSON.parse(e.toString());
+
+    // If type is join_room
+    if (parsedData.type === "join_room") {
+      users.push({
+        ws: socket,
+        roomId: parsedData.roomId,
+      });
+    }
+
+    // If type is message
+    if (parsedData.type === "chat") {
+      let roomUsers = users.filter((u) => u.roomId === parsedData.roomId);
+
+      roomUsers.forEach((u) =>
+        u.ws.send(
+          JSON.stringify({
+            post: {
+              id: parsedData.message.id,
+              content: parsedData.message.content,
+              createdAt: parsedData.message.createdAt,
+              likeCount: parsedData.message.likeCount,
+              commentCount: parsedData.message.commentCount,
+              authorName: parsedData.message.authorName,
+              authorAvatar: parsedData.message.authorAvatar,
+            },
+          })
+        )
+      );
+    }
+
+    // If type is exit_room
+    if (parsedData.type === "exit_room") {
+      users = users.filter((u) => u.ws !== socket);
+    }
+  });
+
+  socket.on("close", () => {
+    users = users.filter((u) => u.ws !== socket);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is listening on ${port}`);
 });
